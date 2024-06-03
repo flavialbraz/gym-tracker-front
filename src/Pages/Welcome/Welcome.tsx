d6da13ec37848bff8031e6692d06054fdcb1ac3f
“Flavia <flaviabrazx@gmail.com>
2024-06-03 19:38:15 -0300

    cadastro page

--- src/Pages/Welcome/Welcome.tsx
+++ src/Pages/Welcome/Welcome.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
-import { Button, Typography } from '@mui/material';
+import { Button, Typography, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { green, grey } from '@mui/material/colors';
 import './style.css';
 
 function Welcome() {
   return (
-    <div className="welcome-screen">
+    <Box className="welcome-screen" display="flex" flexDirection="column" alignItems="center" gap={2} flexWrap="wrap">
       <Typography variant="h4" gutterBottom>
         Bem-vindo! Por favor, faça login para continuar.
       </Typography>
-      <Button variant="contained" color="primary" component={Link} to="/login">
+      <Button
+        variant="contained"
+        component={Link}
+        to="/login"
+        sx={{
+          backgroundColor: green[500],
+          '&:hover': {
+            backgroundColor: green[700],
+          },
+        }}
+      >
         Login
       </Button>
-    </div>
+      <Button
+        variant="contained"
+        component={Link}
+        to="/cadastro"
+        sx={{
+          backgroundColor: green[500],
+          '&:hover': {
+            backgroundColor: green[700],
+          },
+        }}
+      >
+        Cadastro
+      </Button>
+    </Box>
   );
 }
 

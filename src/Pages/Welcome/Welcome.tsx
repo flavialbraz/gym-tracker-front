185c596e2bd1baa7f88dcf010236636ff1611279
“Flavia <flaviabrazx@gmail.com>
2024-06-03 19:45:29 -0300

    home

--- src/Pages/Welcome/Welcome.tsx
+++ src/Pages/Welcome/Welcome.tsx
@@ -3,12 +3,16 @@ import { Button, Typography, Box } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { green, grey } from '@mui/material/colors';
 import './style.css';
-
+import Ilu from '../../img/ilu.png';
 function Welcome() {
   return (
     <Box className="welcome-screen" display="flex" flexDirection="column" alignItems="center" gap={2} flexWrap="wrap">
+      <img src={Ilu} alt="Logo" />
       <Typography variant="h4" gutterBottom>
-        Bem-vindo! Por favor, faça login para continuar.
+        Bem-vindo!
+      </Typography>
+      <Typography variant="body1" sx={{ color: grey[500], marginBottom: 4 }}>
+        O seu tracker fit para acompanhar seus treinos diários.
       </Typography>
       <Button
         variant="contained"
